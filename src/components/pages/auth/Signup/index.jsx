44bb149a7f3244baebebffda0d1ec48678c961e9
K <koka0828us@gmail.com>
2017-04-07 20:41:03 -0700

    Add auth page

---
+++ src/components/pages/auth/Signup/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class Signup extends Component {
+
+  static propTypes = {}
+
+  render() {
+    return (
+      <div>Signup</div>
+    )
+  }
+}
