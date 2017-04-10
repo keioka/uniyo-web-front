d02c5492be855acf4caffda1cfee462668e12533
K <koka0828us@gmail.com>
2017-04-10 11:40:28 -0700

    Add components

--- src/components/index.js
+++ src/components/index.js
@@ -6,6 +6,7 @@ export FAQ from './pages/static/FAQ'
 export Privacy from './pages/static/Privacy'
 export Terms from './pages/static/Terms'
 export InviteFriends from './pages/static/InviteFriends'
+export Index from './pages/static/Index'
 
 /*
   error pages
--- src/components/pages/auth/Signin/index.jsx
+++ src/components/pages/auth/Signin/index.jsx
@@ -4,9 +4,43 @@ export default class Signin extends Component {
 
   static propTypes = {}
 
+  constructor() {
+    super()
+    this.state = {
+      pageIndex: 0
+    }
+  }
+
+  onClick() {
+    this.setState({
+      pageIndex: this.state.pageIndex + 1
+    })
+  }
+
+  onChangeText(event) {
+    console.log(event.target.value)
+  }
+
+  get renderFirstPage() {
+    return (
+      <div>1</div>
+    )
+  }
+
+  get renderSecondPage() {
+    return (
+      <div>2</div>
+    )
+  }
+
   render() {
     return (
-      <div>Signin</div>
+      <div>
+        <div>Signin</div>
+        <input type="text" onChange={::this.onChangeText} />
+        { this.state.pageIndex === 0 ? this.renderFirstPage : this.renderSecondPage}
+        <button onClick={::this.onClick}></button>
+      </div>
     )
   }
 }
