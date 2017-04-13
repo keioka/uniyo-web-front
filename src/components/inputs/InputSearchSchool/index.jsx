5c377ebf5d22482ad29b6ba164f4e90d00705e37
K <koka0828us@gmail.com>
2017-04-13 16:23:04 -0700

    Update Input search school and Add InputTextTransparent to use it on auth pages

--- src/components/inputs/InputSearchSchool/index.jsx
+++ src/components/inputs/InputSearchSchool/index.jsx
@@ -1,17 +1,92 @@
 import React, { Component, PropTypes } from 'react'
 
-const InputSearchSchool = ({ schools, schoolsSearch, onSelectSchool }) => {
+import {
+  InputTextTransparent,
+} from '../../'
 
-  const { data } = schools
+import {
+  containerSearchSchool,
+  itemSchoolActive,
+  listSchool,
+  itemSchool,
+} from './style'
 
-  return (
-    <div>
-      <input type="text" onChange={event => schoolsSearch(event.target.value)} />
-      <ul>
-        {data && data.map(d => <li key={d.id} onClick={() => onSelectSchool(d)}>{d.name}</li>)}
-      </ul>
-    </div>
-  )
+
+const DOWN = "ArrowDown"
+const UP = "ArrowUp"
+const ENTER = "Enter"
+
+export default class InputSearchSchool extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentIndex: -1,
+    }
+  }
+
+  componentWillReceiveProps() {
+    this.setState({
+      currentIndex: -1
+    })
+  }
+
+
+  onKeyEventhandler(event) {
+    const { schoolsSearch, onSelectSchool, schools } = this.props
+    const { currentIndex } = this.state
+    const dataLenght = schools.data.length
+
+    if (event.key === DOWN) {
+      this.setState({
+        currentIndex: (currentIndex >= -1 && currentIndex < dataLenght - 1) ?
+          currentIndex + 1:
+          currentIndex
+      })
+    }
+
+    if (event.key === UP) {
+      this.setState({
+        currentIndex: (currentIndex > -1 && currentIndex <= dataLenght - 1) ?
+          currentIndex - 1:
+          currentIndex
+      })
+    }
+
+    if (event.key === ENTER) {
+      const { onSelectSchool, schools } = this.props
+      const selectedSchool = schools.data[this.state.currentIndex]
+      onSelectSchool(selectedSchool)
+    }
+  }
+
+  render() {
+    const { schoolsSearch, onSelectSchool, schools } = this.props
+    const { data } = schools
+    return (
+      <div className={containerSearchSchool}>
+        <InputTextTransparent
+          onChange={schoolsSearch}
+          placeholder="Type your school name"
+          onKeyDown={event => ::this.onKeyEventhandler(event)}
+        />
+        <ul className={listSchool} >
+          {data.length > 0 && data.map((school, index) => {
+            const classNames = index === this.state.currentIndex ? `${itemSchool} ${itemSchoolActive}` : itemSchool
+            return (
+              <li
+                key={school.id}
+                className={classNames}
+                onClick={() => onSelectSchool(school)}
+              >
+                {school.name}
+              </li>
+            )
+
+            }
+          )}
+        </ul>
+      </div>
+    )
+  }
 }
-
-export default InputSearchSchool
