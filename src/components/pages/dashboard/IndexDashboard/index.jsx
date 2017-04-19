b3a07906a7d055ef358b46b71af0e79a1363dc8c
K <koka0828us@gmail.com>
2017-04-19 14:08:31 -0700

    Add fetch post and filter of posts

--- src/components/pages/dashboard/IndexDashboard/index.jsx
+++ src/components/pages/dashboard/IndexDashboard/index.jsx
@@ -1,3 +1,4 @@
+/* @flow */
 import React, { Component, PropTypes } from 'react'
 
 import {
@@ -5,12 +6,64 @@ import {
   CardPost,
 } from '../../../index'
 
-
 export default class IndexDashboard extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      isLoadingMorePost: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', ::this.onScrollHandler)
+  }
+
+  onScrollHandler(event) {
+    const dashboard = this._dashboard
+    const { posts } = this.props
+    const lastPost = posts[posts.length - 1]
+    const { scrollHeight } = event.target.body
+    const currentHeight = event.target.body.scrollTop + window.screen.availHeight
+
+    if (
+      scrollHeight < currentHeight &&
+      !this.state.isLoadingMorePost &&
+      lastPost // to avoid bug 'lastPost returns undefined' while scrolling
+    ) {
+
+      // TODO: fix bug 'this.props.postsSearch action dispatched twice'
+      this.setState({
+        isLoadingMorePost: true,
+      }, () => {
+        const params = {lastPostId: lastPost.id}
+        params.hashtags = this.props.hashtag && [this.props.hashtag]
+        params.types = this.props.type && [this.props.type]
+        this.props.postsSearch(params)
+      })
+    }
+  }
+
+  componentWillReceiveProps() {
+    this.setState({
+      isLoadingMorePost: false,
+    })
+  }
+
   render() {
     return (
-      <div>
-        {this.props.allPosts.map(post => <CardPost {...post} />)}
+      <div ref={(div)=> this._dashboard = div}>
+        {this.props.posts.map((post) => {
+          const comments = this.props.allComments.filter(comment => comment.postId === post.id)
+          return (
+            <CardPost
+              {...post}
+              commentsSearch={this.props.commentsSearch}
+              comments={comments}
+            />
+          )
+        })}
+        { this.state.isLoadingMorePost && <div> Loading </div> }
       </div>
     )
   }
--- src/containers/Dashboard/index.jsx
+++ src/containers/Dashboard/index.jsx
@@ -1,6 +1,6 @@
 /* @flow */
 
-import React, { Component, PropTypes } from 'react'
+import React, { Component, PureComponent, PropTypes } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { actions } from 'uniyo-redux'
@@ -17,17 +17,25 @@ import {
   main,
   header,
   mainContent,
+  footer,
+  barNoification,
 } from './style'
 
 const mapStateToProps = state => ({
   auth: state.auth,
   post: state.post,
+  comment: state.comment,
 })
 
-const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({
+  postsSearch: actions.postsSearch,
+  commentsSearch: actions.commentsSearch,
+}, dispatch)
+
+const regexTag = /#([ÂÃÄÀÁÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿa-zA-Z0-9-]+)/g
 
 @connect(mapStateToProps, mapDispatchToProps)
-export default class DashBoard extends Component {
+export default class DashBoard extends PureComponent {
 
   static propTypes = {}
 
@@ -38,6 +46,13 @@ export default class DashBoard extends Component {
     }
   }
 
+  componentWillMount() {
+    const { hashtag } = this.props.location.query
+    this.setState({
+      currentHashTag: hashtag,
+    })
+  }
+
   onSelectPostType(type) {
     this.setState({
       currentPostType: type
@@ -45,11 +60,54 @@ export default class DashBoard extends Component {
   }
 
   get renderContent() {
-    const { hashtags } = this.props.auth.currentUser
-    const { all } = this.props.post
+    const {
+      commentsSearch,
+      postsSearch,
+      post,
+      comment,
+      auth,
+    } = this.props
+
+
+    const TYPES = {
+      docs: 'CLASS_NOTE',
+      post: 'POST',
+      reviews: 'REVIEW',
+      questions: 'QUESTION',
+    }
+
+    const { hashtags } = auth.currentUser
+    const { all: allPosts } = post
+    const { all: allComments } = comment
+    const { hashtag } = this.props.location.query
+    const { type } = this.props.location.query
+    let sortedPosts = allPosts
+
+    if (hashtag) {
+      postsSearch({hashtags: [hashtag]})
+      sortedPosts = sortedPosts.filter(post => {
+        const hashtag:String = `#${this.props.location.query.hashtag}`
+        const matched:Array = post.text.match(regexTag) || []
+        return matched.includes(hashtag)
+      })
+    }
+
+    if (type) {
+      sortedPosts = sortedPosts.filter(post => {
+        console.log(post.type)
+        return post.postType === TYPES[type]
+      })
+    }
+
+    console.log(sortedPosts)
 
     const childComponents = React.Children.map(this.props.children, child => React.cloneElement(child, {
-      allPosts: all
+      posts: sortedPosts,
+      hashtag,
+      type: TYPES[type],
+      allComments,
+      postsSearch,
+      commentsSearch,
     }))
 
     return (
@@ -59,10 +117,11 @@ export default class DashBoard extends Component {
           <header className={header}>
             <NavPostType onSelectPostType={::this.onSelectPostType} currentPostType={this.state.currentPostType} />
           </header>
+          { hashtag && <div className={barNoification}>#{hashtag}</div>}
           <div className={mainContent}>
             {childComponents}
           </div>
-          <footer>footer</footer>
+          <footer className={footer}></footer>
         </div>
       </div>
     )
@@ -76,9 +135,11 @@ export default class DashBoard extends Component {
 
   render() {
     const { fetching } = this.props.post
+
+    // TODO: fetching case
     return (
       <LayoutDashboard>
-        { fetching ? this.renderLoading : this.renderContent }
+        { this.renderContent }
       </LayoutDashboard>
     )
   }
