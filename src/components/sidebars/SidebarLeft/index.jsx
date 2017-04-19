0317034192b9e3c817df7c484db88d3fb34aa71a
K <koka0828us@gmail.com>
2017-04-19 14:11:29 -0700

    Modify UI design

--- src/components/sidebars/SidebarLeft/index.jsx
+++ src/components/sidebars/SidebarLeft/index.jsx
@@ -1,15 +1,25 @@
 import React, { Component, PropTypes } from 'react'
+import { Link } from 'react-router'
 
 import {
   wrapper,
   newsfeed,
-  newsfeedTag
+  newsfeedTag,
+  newsfeedLabel,
 } from './style'
 
 export default ({ hashtags }) => (
   <aside className={wrapper} >
+    <input type="text"/>
     <ul className={newsfeed}>
-      { hashtags && hashtags.map(hashtag => <li key={hashtag.hashtag} className={newsfeedTag}>#{hashtag.hashtag}</li>) }
+      <h4 className={newsfeedLabel}>News Feed</h4>
+      { hashtags && hashtags.map(hashtag => <li><Link key={hashtag.hashtag} to={`dashboard?hashtag=${hashtag.hashtag}`} className={newsfeedTag}>#{hashtag.hashtag}</Link></li>) }
+      <li><Link to={`dashboard?hashtag=kei`} className={newsfeedTag}>#ke</Link></li>
+      <li><Link to={`dashboard?hashtag=kei`} className={newsfeedTag}>#ke</Link></li>
+      <li><Link to={`dashboard?hashtag=kei`} className={newsfeedTag}>#ke</Link></li>
+      <li><Link to={`dashboard?hashtag=kei`} className={newsfeedTag}>#ke</Link></li>
+      <li><Link to={`dashboard?hashtag=kei`} className={newsfeedTag}>#ke</Link></li>
     </ul>
+    <div>Signout</div>
   </aside>
 )
