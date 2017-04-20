f321cefeec67f0edb206ef290b435f86b7d38371
K <koka0828us@gmail.com>
2017-04-19 17:57:43 -0700

    Add new Text components

--- src/components/texts/TextPost/index.jsx
+++ src/components/texts/TextPost/index.jsx
@@ -1,17 +1,38 @@
 import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
 import reactStringReplace from 'react-string-replace'
+import ReactEmoji from 'react-emoji'
+
+import {
+  TextMention,
+} from '../../'
 
 import {
   hashtag,
-} from './'
+} from './style'
 
 export default ({ text }) => {
-  const parsedText = reactStringReplace(text, /#([ÂÃÄÀÁÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿa-zA-Z0-9-]+)/g, (tag, i) => {
+
+  let parsedText = ReactEmoji.emojify(text)
+
+  parsedText = reactStringReplace(parsedText, /#([ÂÃÄÀÁÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿa-zA-Z0-9-]+)/g, (tag, i) => {
     return (
-      <Link to={`/dashboard?hashtag=${tag}`}>#{tag}</Link>
+      <Link to={`/dashboard?hashtag=${tag}`} className={hashtag}>#{tag}</Link>
     )
   })
+
+  parsedText = reactStringReplace(parsedText , /<@(.*?)>/g, (match, i) => {
+    const segments = match
+                      .replace('<@', '')
+                      .replace('>', '')
+                      .split('|')
+
+    const userId = segments[0]
+    const display = segments[1]
+
+    return <TextMention key={`MENTION_${i}`} userId={userId} display={display} />
+  })
+
   return (
     <span>{parsedText}</span>
   )
