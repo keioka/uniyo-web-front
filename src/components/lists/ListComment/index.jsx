17066456bbc7ec2dedf92ce8f03a5af82adb2255
K <koka0828us@gmail.com>
2017-04-21 13:50:34 -0700

    Update ListComment

--- src/components/lists/ListComment/index.jsx
+++ src/components/lists/ListComment/index.jsx
@@ -1,12 +1,24 @@
 import React, { Component, PropTypes } from 'react'
 
 import {
+  boxImage,
+  fontName,
+  imgUser,
+  wrapper,
 } from './style'
 
-export default (props) => {
+const ListComment = ({ id, user, text }) => {
+  console.log(user)
   return (
-    <li>
-      ded
+    <li key={id} className={wrapper}>
+      <span className={boxImage}>
+        <img src={user.image.smallUrl} className={imgUser} />
+      </span>
+      <span className={fontName}>{user.firstName}</span>
+      <span>{text}  ttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttext ttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttextttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttextttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttextttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttextttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttextttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttextttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttextttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttextttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttextttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttext ttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttextttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttextttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttextttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttext ttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttextttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttext ttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttext ttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttext ttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttext ttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttext ttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttextttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttext ttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttextttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextexttext</span>
     </li>
   )
+  // TODO: what if long comment
 }
+
+export default ListComment
