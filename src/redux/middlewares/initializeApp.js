b2513fe0dc44adae30052cc224d381cf9fee8d5e
K <koka0828us@gmail.com>
2017-04-21 13:55:49 -0700

    Add middleware to initialize current user

---
+++ src/redux/middlewares/initializeApp.js
@@ -0,0 +1,23 @@
+import { actionTypes } from 'uniyo-redux'
+import { actions } from 'uniyo-redux'
+import { storage } from '../../utils'
+import { browserHistory } from 'react-router'
+import { put } from 'redux-saga/effects'
+export const initializeApp = store => next => action => {
+
+  if (!localStorage) {
+    console.warn("Local Storage is not available")
+  }
+
+  // fetch current user info to auth reducer
+  if (action.type === actionTypes.tokenRefresh.success) {
+    const userId = JSON.parse(storage.user).id
+    const { accessToken } = storage
+    store.dispatch(actions.currentUser({
+      userId,
+      accessToken
+    }))
+  }
+
+  return next(action)
+}
