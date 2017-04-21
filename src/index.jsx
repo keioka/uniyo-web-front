ac89029f322c7a5b4a421613d8b99d7bba5c5430
K <koka0828us@gmail.com>
2017-04-21 14:16:16 -0700

    Remove store and middleware from index file

--- src/index.jsx
+++ src/index.jsx
@@ -1,30 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import createSagaMiddleware from 'redux-saga'
 import AppRouter from './routes.jsx'
-
-import { reducers } from 'uniyo-redux'
-import { actions } from 'uniyo-redux'
-import rootSaga from './redux/sagas/root'
-import { accessTokenValidator } from './redux/middlewares/accessTokenValidator'
-import { redirectHandler } from './redux/middlewares/redirectHandler'
-import { composeWithDevTools } from 'redux-devtools-extension'
-
 import { storage } from './utils/index'
-
-const sagaMiddleware = createSagaMiddleware()
-
-function configureStore() {
-  const createStoreWithMiddleware = composeWithDevTools(
-    applyMiddleware(sagaMiddleware, accessTokenValidator, redirectHandler),
-  )(createStore)
-  return createStoreWithMiddleware(reducers)
-}
-
-const store = configureStore()
-sagaMiddleware.run(rootSaga)
+import store from './redux/store'
 
 render(
   <Provider store={store}>
