45885918f50fbadb2c7927e8cc082abebaea2acb
K <koka0828us@gmail.com>
2017-04-21 13:53:43 -0700

    Modify Nav link

--- src/components/navs/NavPostType/index.jsx
+++ src/components/navs/NavPostType/index.jsx
@@ -6,26 +6,52 @@ import {
   itemActive,
 } from './style'
 
+const types = [
+  { id: 1, name: 'ALL', title: 'All posts', path: null },
+  { id: 2, name: 'REVIEW', title: 'Reviews', path: 'reviews' },
+  { id: 3, name: 'QUESTION', title: 'Questions', path: 'questions' },
+  { id: 4, name: 'DOC', title: 'Documents', path: 'docs' },
+]
+
+
+const dashboardPathGenarator = ({ hashtag, type }) => {
+  let path = 'dashboard'
+
+  if (hashtag || type) {
+    path += '?'
+  }
+
+  if (hashtag) {
+    path += `hashtag=${hashtag}`
+  }
+
+  if (hashtag && type) {
+    path += '&'
+  }
+
+  if (type) {
+    path += `type=${type}`
+  }
+
+  return path
+}
+
+
 export default ({ currentPostType, onSelectPostType, currentHashTag }) => {
-  const types = [
-    { id: 1, name: 'ALL', title: 'All posts', path: '/dashboard'},
-    { id: 2, name: 'REVIEW', title: 'Reviews', path: '/dashboard?type=reviews'},
-    { id: 3, name: 'QUESTION', title: 'Questions', path: '/dashboard?type=questions'},
-    { id: 4, name: 'DOC', title: 'Documents', path: '/dashboard?type=docs'},
-  ]
   return (
     <ul className={wrapper}>
-      {types.map(type => {
-        let classNames = [item]
+      {types.map((type) => {
+        const classNames = [item]
         if (type.name === currentPostType) {
           classNames.push(itemActive)
         }
+        const path = dashboardPathGenarator({ hashtag: currentHashTag, type: type.path })
         return (
-          <li key={type.id}
-            className={classNames.join(" ")}
-            onClick={() => onSelectPostType(type.name)}
+          <li
+            key={type.id}
+            className={classNames.join(' ')}
           >
-            <Link to={`${type.path}?hashtags=${currentHashTag}`}>{type.title}</Link>
+            <Link to={path}>{type.title}</Link>
           </li>
         )
       })}
