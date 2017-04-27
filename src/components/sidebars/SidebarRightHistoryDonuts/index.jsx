4fb2dcbc52618e4d3303ac979f096db80aeb5fb4
K <koka0828us@gmail.com>
2017-04-26 17:06:07 -0700

    Add sidebar right components and modify sidebar left css

---
+++ src/components/sidebars/SidebarRightHistoryDonuts/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from 'react'
+import { Link } from 'react-router'
+
+import {
+  wrapper,
+} from './style'
+
+export default () => {
+  return (
+    <div className={wrapper} >
+
+    </div>
+  )
+}
