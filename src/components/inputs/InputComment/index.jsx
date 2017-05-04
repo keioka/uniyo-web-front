741568e57fcba8df9adb5fcfb1ce1d1827ba3165
K <koka0828us@gmail.com>
2017-05-04 09:25:08 -0700

    Update InputComment to add st.js

--- src/components/inputs/InputComment/index.jsx
+++ src/components/inputs/InputComment/index.jsx
@@ -1,19 +1,31 @@
 import React, { Component, PropTypes } from 'react'
 
 import {
-  inputText
+  input,
+  inputBoxImg,
+  wrapper,
 } from './style'
+
+import localStorage from '../../../utils/localStorageHandler'
+import postTranspiler from '../../../utils/postTranspiler'
+
+import $ from 'jquery'
+import 'jquery.caret'
+import 'style-loader!css-loader!at.js/dist/css/jquery.atwho.css'
+import 'at.js'
 
 export default class InputComment extends Component {
 
   onKeyDownHandler = (event) => {
+    const { postId, commentCreate } = this.props
+    const text = postTranspiler(this._inputComment)
     if (event.key === 'Enter'){
-      commentCreate({ postId , text: event.target.value })
+      commentCreate({ postId , text: text})
     }
   }
 
   componentDidMount() {
-    $('#input').atwho({
+    $(this._inputComment).atwho({
       at: "@",
       callbacks: {
         remoteFilter: function(query, callback) {
@@ -40,14 +52,16 @@ export default class InputComment extends Component {
     })
   }
   render() {
-    const { postId, className, commentCreate } = this.props
-    const classNames = `${inputText} ${className}`
+    const { postId, className, commentCreate, currentUser } = this.props
+    const classNames = `${input} ${className}`
 
     return (
-      <span>
-        <div
+      <span className={wrapper}>
+        <span className={inputBoxImg}><img src={`${currentUser.image.smallUrl}`} alt=""/></span>
+        <span
+          ref={ref => this._inputComment = ref }
           contentEditable={true}
-          className={inputText}
+          className={input}
           data-user-image={"https://uniyo.s3.amazonaws.com/users/profile/small/541_f53b1f4364a4415ebdbda4dd0af1ca51.jpg"}
           onKeyDown={::this.onKeyDownHandler}
           type="text"
