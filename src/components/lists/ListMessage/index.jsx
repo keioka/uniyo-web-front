a5b3d5c4d6e16aea21659145dcfaf611d117999c
K <koka0828us@gmail.com>
2017-05-09 17:49:21 -0700

    Update to merge messages of same users

--- src/components/lists/ListMessage/index.jsx
+++ src/components/lists/ListMessage/index.jsx
@@ -9,16 +9,22 @@ import {
   fontName,
   imgUser,
   wrapper,
+  sectionContent,
+  paragaph,
 } from './style'
 
-const ListMessage = ({ id, user, text }) => {
+const ListMessage = ({ messages, showUserInfo }) => {
+  const message = messages[0]
+  const { id, user } = message
   return (
     <li key={id} className={wrapper}>
-      <span className={boxImage}>
+      <div className={boxImage}>
         <img src={user.image.smallUrl} className={imgUser} />
-      </span>
-      <span className={fontName}>{user.firstName}</span>
-      <p><TextPost text={text} /></p>
+      </div>
+      <div className={sectionContent}>
+        <span className={fontName}>{user.firstName}</span>
+        {messages.map(message => <p className={paragaph}><TextPost text={message.text} showUserInfo={showUserInfo} /></p>)}
+      </div>
     </li>
   )
   // TODO: what if long comment
