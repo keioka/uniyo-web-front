153da86f4f8fadd2d4b8e9093584d2b1169c0b7d
K <koka0828us@gmail.com>
2017-05-10 18:10:29 -0700

    Add event channel to websocket saga

--- src/redux/sagas/websocket.js
+++ src/redux/sagas/websocket.js
@@ -1,13 +1,13 @@
 import { put, take, fork, call } from 'redux-saga/effects'
+import { eventChannel, END } from 'redux-saga'
 import uiActionTypes from '../actionTypes'
 import { actionTypes } from 'uniyo-redux'
 import converter from 'json-style-converter/es5'
 
 const webSocketUrl = 'wss://live.uniyo.io/v1/ws'
 
-function connect () {
+function connect() {
   const socket = new WebSocket(webSocketUrl)
-
   return new Promise(resolve => {
     resolve(socket)
   })
@@ -32,22 +32,39 @@ function sendMessage(msg) {
   this.ws.send(JSON.stringify(snakeCaseMsg))
 }
 
-function onMessage(event) {
-  let data = JSON.parse(event.data)
-  const { type } = data
-  data = converter.snakeToCamelCase(data)
+// function onMessage
 
-  switch (type) {
-    case "SOCKET_READY": {
-      
+function subscribe(socket) {
+  return eventChannel(emit => {
+
+    socket.onmessage = (event) => {
+      console.log(event)
+      let data = JSON.parse(event.data)
+      const { type } = data
+      data = converter.snakeToCamelCase(data)
+      switch (type) {
+        case "SOCKET_READY": {
+          emit({ type: "WEBSOCKET/READY", data })
+        }
+      }
     }
-  }
+
+    return () => {
+      socket.close()
+    }
+  })
 }
 
 function* flow() {
   // get initialized web socket
   const socket = yield call(connect)
-  socket.onmessage = onMessage
+  const channel = yield call(subscribe, socket)
+
+  while (true) {
+    const action = yield take(channel)
+    yield put(action)
+    console.log('take: ', action)
+  }
 }
 
 export default function* webSocket() {
