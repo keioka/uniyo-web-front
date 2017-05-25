c30af8d1fc44dba697f377aa05b4c050521b50dc
K <koka0828us@gmail.com>
2017-05-25 12:06:50 -0700

    Modify UI of sidebar left

--- src/components/sidebars/SidebarLeft/index.jsx
+++ src/components/sidebars/SidebarLeft/index.jsx
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import { browserHistory, Link } from 'react-router'
 import {
   InputSearchTag,
+  ListHashtag,
 } from '../../'
 
 import {
@@ -24,43 +25,15 @@ import {
 } from './style'
 
 import Plus from './plus-active'
-import Close from './close'
-
-const dashboardPathGenarator = ({ hashtag, type }) => {
-  let path = '/dashboard'
-
-  if (hashtag || type) {
-    path += '?'
-  }
-
-  if (hashtag) {
-    path += `hashtag=${hashtag}`
-  }
-
-  if (hashtag && type) {
-    path += '&'
-  }
-
-  if (type) {
-    path += `type=${type}`
-  }
-
-  return path
-}
-
-function hashCode(str) {
-  return str.split('').reduce((prevHash, currVal) =>
-    ((prevHash << 5) - prevHash) + currVal.charCodeAt(0), 0)
-}
 
 export default class SidebarLeft extends Component {
 
-   state = {
-     keywordForSort: '',
-     isShowInputAddTag: false,
-     isShowMoreTags: false,
-     isShowMoreChannels: false,
-   }
+  state = {
+    keywordForSort: '',
+    isShowInputAddTag: false,
+    isShowMoreTags: false,
+    isShowMoreChannels: false,
+  }
 
   onClickBtnAddHashTag() {
     this.setState({
@@ -84,160 +57,153 @@ export default class SidebarLeft extends Component {
   }
 
   get navSideBar() {
-
     const MAX_NUMBER_SHOW_ITEM = 4
     const { keywordForSort } = this.state
     const { allChannels, hashtagsCurrentUser, hashtagsTrending, hashtagDelete } = this.props
-
-    const ListHashtag = ({ className, hashtag, hashtagType, type, showBtnDelete, hashtagDelete }) => (
-        <li className={sectionTag}>
-          <Link
-            className={className}
-            key={hashCode(hashtag)}
-            to={dashboardPathGenarator({ hashtag })}
-          >
-            <span className={tag}>#{hashtag}</span>
-          </Link>
-          { showBtnDelete &&
-            <span
-              className={tagBtnClose}
-              onClick={(event) => { hashtagDelete({ hashtag, hashtagType }); event.stopPropagation() }}
-            >
-              <Close />
-            </span>
-          }
-        </li>
-    )
 
     const ListChannel = ({ className, channel }) => (
       <Link
         className={className}
         key={channel.id}
         to={`/dashboard/channels/${channel.id}`}
-      >
-        <li className={sectionTag}>
-          {channel.users.length > 1 ?
-            (<span data-amount-users={channel.users.length} className={iconChannel}>
-              {channel.users.length}
-            </span>): (<span data-user-online={true} className={iconChannelOnlineStatus}><span className={iconOnline}></span></span>)
+        >
+          <li className={sectionTag}>
+            {channel.users.length > 1 ?
+              (<span data-amount-users={channel.users.length} className={iconChannel}>
+                {channel.users.length}
+              </span>) : (<span data-user-online className={iconChannelOnlineStatus}><span className={iconOnline} /></span>)
+            }
+            <span className={userNames}>{channel.users.map(user => user.name.split(' ')[0]).join(', ')}</span>
+          </li>
+        </Link>
+      )
+
+      const ComponentsHashtag = hashtagsCurrentUser &&
+      Array.from(new Set(hashtagsCurrentUser)).filter(hashtag =>
+        hashtag.hashtag.toLowerCase().includes(keywordForSort)).map((hashtag, index) => {
+          const classNames = []
+          if (!this.state.isShowMoreTags && index > MAX_NUMBER_SHOW_ITEM) {
+            classNames.push(hide)
           }
-          <span className={userNames}>{channel.users.map(user => user.name.split(" ")[0]).join(', ')}</span>
-        </li>
-      </Link>
-    )
 
-    const ComponentsHashtag = hashtagsCurrentUser && Array.from(new Set(hashtagsCurrentUser)).filter(hashtag => hashtag.hashtag.toLowerCase().includes(keywordForSort)).map((hashtag, index) => {
-      let classNames = []
-      if (!this.state.isShowMoreTags && index > MAX_NUMBER_SHOW_ITEM) {
-        classNames.push(hide)
-      }
-      return (
-        <ListHashtag
-          className={classNames.join(' ')}
-          hashtag={hashtag.hashtag}
-          hashtagType={hashtag.type}
-          hashtagDelete={hashtagDelete}
-          showBtnDelete
-          type={this.props.type}
-        />
-      )
-    })
-
-    const ComponentsChannel = allChannels && allChannels.filter(channel => channel.users[0].name.toLowerCase().includes(keywordForSort)).map((channel, index) => {
-      let classNames = []
-      if (!this.state.isShowMoreChannels && index > MAX_NUMBER_SHOW_ITEM) {
-        classNames.push(hide)
-      }
-      return (
-        <ListChannel
-          className={classNames.join(' ')}
-          channel={channel}
-        />
-      )
-    })
-
-    const ComponentsTrendingHashtag = hashtagsTrending && hashtagsTrending.filter(hashtag => hashtag.toLowerCase().includes(keywordForSort)).map((hashtag, index) => {
-      let classNames = []
-      if (!this.state.isShowMoreTags && index > MAX_NUMBER_SHOW_ITEM) {
-        classNames.push(hide)
-      }
-      return (
-        <ListHashtag
-          className={classNames.join(' ')}
-          hashtag={hashtag}
-          type={this.props.type}
-        />
-      )
-    })
-
-    return (
-      <nav>
-        <ul className={section}>
-          <h4 className={sectionLabel} onClick={::this.onClickBtnAddHashTag}><span>News Feed</span><Plus /></h4>
-          { this.state.isShowInputAddTag &&
-            <input
-              type="text"
-              className={inputAddTag}
-              ref={(ref) => this._inputAddTag = ref}
-              onKeyUp={::this.onSubmitAddTag}
+          return (
+            <ListHashtag
+              className={classNames.join(' ')}
+              hashtag={hashtag.hashtag}
+              hashtagType={hashtag.type}
+              hashtagDelete={hashtagDelete}
+              showBtnDelete
+              type={this.props.type}
             />
-          }
-          { hashtagsCurrentUser && ComponentsHashtag }
-          { keywordForSort === '' &&
-            hashtagsCurrentUser &&
-            hashtagsCurrentUser.length > MAX_NUMBER_SHOW_ITEM &&
-            <button
-              className={btnShowMore}
-              onClick={() => { this.setState({ isShowMoreTags: !this.state.isShowMoreTags }) }}
-            >
-              {this.state.isShowMoreTags ? 'Hide' : 'Show more'}
-            </button>
-          }
-          <h4 className={sectionTextAdd} onClick={::this.onClickBtnAddHashTag}>+ Add a new hashtag</h4>
-        </ul>
+          )
+        })
 
-        <ul className={section}>
-          <h4 className={sectionLabel}>TRENDING TOPIC</h4>
-          {hashtagsTrending && ComponentsTrendingHashtag}
-        </ul>
+        const ComponentsChannel = allChannels &&
+        allChannels.filter(channel =>
+          channel.users[0].name.toLowerCase().includes(keywordForSort)).map((channel, index) => {
+            let classNames = []
+            if (!this.state.isShowMoreChannels && index > MAX_NUMBER_SHOW_ITEM) {
+              classNames.push(hide)
+            }
+            return (
+              <ListChannel
+                className={classNames.join(' ')}
+                channel={channel}
+              />
+            )
+          })
 
-        <ul className={section}>
-          <Link to='/dashboard/channels/new'>
-            <h4 className={sectionLabel}>
-              PRIVATE MESSAGES <Plus />
-            </h4>
-          </Link>
-          {allChannels && ComponentsChannel}
-          <Link to='/dashboard/channels/new'><h4 className={sectionTextAdd}>+ Start a new chat</h4></Link>
-          { keywordForSort === '' &&
-            allChannels &&
-            allChannels.length > MAX_NUMBER_SHOW_ITEM &&
-            <button
-              className={btnShowMore}
-              onClick={() => { this.setState({ isShowMoreChannels: !this.state.isShowMoreChannels }) }}
-            >
-              {this.state.isShowMoreChannels ? 'Hide' : 'Show more'}
-            </button>
-          }
-        </ul>
-      </nav>
-    )
+          const ComponentsTrendingHashtag = hashtagsTrending && hashtagsTrending.filter(hashtag => hashtag.toLowerCase().includes(keywordForSort)).map((hashtag, index) => {
+            const classNames = []
+            if (!this.state.isShowMoreTags && index > MAX_NUMBER_SHOW_ITEM) {
+              classNames.push(hide)
+            }
+            return (
+              <ListHashtag
+                className={classNames.join(' ')}
+                hashtag={hashtag}
+                type={this.props.type}
+              />
+            )
+          })
+
+          return (
+            <nav>
+              <ul className={section}>
+                <h4 className={sectionLabel} onClick={::this.onClickBtnAddHashTag}><span>News Feed</span><Plus /></h4>
+                { this.state.isShowInputAddTag &&
+                  <input
+                    type="text"
+                    className={inputAddTag}
+                    ref={(ref) => this._inputAddTag = ref}
+                    onKeyUp={::this.onSubmitAddTag}
+                  />
+                }
+                { hashtagsCurrentUser && ComponentsHashtag }
+                { keywordForSort === '' &&
+                hashtagsCurrentUser &&
+                hashtagsCurrentUser.length > MAX_NUMBER_SHOW_ITEM &&
+                <button
+                  className={btnShowMore}
+                  onClick={() => { this.setState({ isShowMoreTags: !this.state.isShowMoreTags }) }}
+                  >
+                    {this.state.isShowMoreTags ? 'Hide' : 'Show more'}
+                  </button>
+                }
+                <h4
+                  className={sectionTextAdd}
+                  onClick={::this.onClickBtnAddHashTag}
+                  >
+                    <span>+ Add a new hashtag</span>
+                  </h4>
+                </ul>
+
+                <ul className={section}>
+                  <h4 className={sectionLabel}>TRENDING TOPIC</h4>
+                  {hashtagsTrending && ComponentsTrendingHashtag}
+                </ul>
+
+                <ul className={section}>
+                  <Link to='/dashboard/channels/new'>
+                  <h4 className={sectionLabel}>
+                    PRIVATE MESSAGES <Plus />
+                  </h4>
+                </Link>
+                {allChannels && ComponentsChannel}
+                <Link to='/dashboard/channels/new'>
+                <h4 className={sectionTextAdd}>
+                  <span>+ Start a new chat</span>
+                </h4>
+              </Link>
+              { keywordForSort === '' &&
+              allChannels &&
+              allChannels.length > MAX_NUMBER_SHOW_ITEM &&
+              <button
+                className={btnShowMore}
+                onClick={() => { this.setState({ isShowMoreChannels: !this.state.isShowMoreChannels }) }}
+                >
+                  {this.state.isShowMoreChannels ? 'Hide' : 'Show more'}
+                </button>
+              }
+            </ul>
+          </nav>
+        )
+      }
+
+    render() {
+      return (
+        <aside className={wrapper} >
+          <InputSearchTag
+            className={inputSearchTag}
+            onChange={event => this.setState({ keywordForSort: event.target.value })}
+          />
+          <ul className={section}>
+            <h3 className={`${sectionTag} ${sectionTagHot}`}>
+              <Link to="/dashboard/posts/top">All in EDHECBUSINES</Link>
+            </h3>
+          </ul>
+          {this.navSideBar}
+        </aside>
+      )
+    }
   }
-
-  render() {
-    return (
-      <aside className={wrapper} >
-        <InputSearchTag
-          className={inputSearchTag}
-          onChange={event => this.setState({ keywordForSort: event.target.value })}
-        />
-        <ul className={section}>
-          <h3 className={`${sectionTag} ${sectionTagHot}`}>
-            <Link to="/dashboard/posts/top">All in EDHECBUSINES</Link>
-          </h3>
-        </ul>
-        {this.navSideBar}
-      </aside>
-    )
-  }
-}
