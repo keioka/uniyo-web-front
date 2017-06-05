58ee53da4fc64ec25beea3b3badd3e1080ddaa3c
K <koka0828us@gmail.com>
2017-06-05 11:27:19 -0700

    Modify push notification

--- public/notification_sw.js
+++ public/notification_sw.js
@@ -1,48 +1,24 @@
+
 var notificationHandler = (function(){
 
-  function showNotification(title, notification){
-    return self.registration.showNotification(title, notification);
-  }
-
-  function showPMNotification(name, data){
-    var title = "New message from " + name;
-    var actions = [
-      {action: "like", title: "Like"},
-      {action: "reply", title: "Reply"}
-    ];
-
-    // TODO: [Add] more notification body
-    var notfication = {
-      body: "Hi, thank you!",
-      actions: actions,
-      //  icon: '/app/images/image.jpg',
-      //  sound: '/app/res/raw/14044860.mp3',
-      //  vibrate: [200, 100, 200, 100, 200, 100, 200],
-      //  tag: 'my-tag'
+  function showNotification(notification){
+    var title = ""
+    var notificationOptions = {
+      body: notification.message,
+      icon: '/assets/img/uniyo.png'
     }
 
-    return showNotification(title, notfication);
+    return self.registration.showNotification(notification.title, notificationOptions);
   }
 
-  function showMentionedNotification(feedName, data){
-    var title = "You are mentioned at " + feedName;
-    var notfication = {
-      body: "Hello",
-      actions: [
-        {action: "like", title: "Like"},
-        {action: "reply", title: "Reply"}
-      ]
+  function push(e){
+    if (e.data) {
+      var n = e.data.json();
+      return showNotification(n);
     }
-
-    return showNotification(title, notfication);
-  }
-
-  function push(event){
     // TODO: [Add]
     //   - check the type of notification
     //   - call function depending on notification type
-
-    return showPMNotification("Kei"); // This is test
   }
 
   return {
@@ -51,13 +27,8 @@ var notificationHandler = (function(){
 
 }());
 
-self.addEventListener('install', function(event) {
-  console.log("SW installed");
-});
-
 self.addEventListener("push", event => {
   // TODO: [Add] Notification type
-  console.log(event)
   notificationHandler.push(event);
 });
 
@@ -66,15 +37,5 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("notificationclick", function(event) {
-  var channelId = "412";
-
-  event.notification.close();
-
-  if (event.action === "like") {
-    // TODO: add like to comment
-  } else if (event.action === "reply") {
-    // TODO: add reply to comment
-    clients.openWindow("/dashboard/channels/" + channelId);
-  }
-
+  clients.openWindow("/dashboard/notifications/");
 }, false);
