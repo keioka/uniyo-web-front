93834936cc485f91964a6b15b05eca83cf3bbd89
K <koka0828us@gmail.com>
2017-06-12 01:30:52 -0700

    Modify InputPost

--- src/components/inputs/InputPost/index.jsx
+++ src/components/inputs/InputPost/index.jsx
@@ -27,6 +27,24 @@ import {
   btnFileDelete,
 } from './style'
 
+function placeCaretAtEnd(el) {
+    el.focus()
+    if (typeof window.getSelection != "undefined"
+            && typeof document.createRange != "undefined") {
+        var range = document.createRange()
+        range.selectNodeContents(el)
+        range.collapse(false)
+        var sel = window.getSelection()
+        sel.removeAllRanges()
+        sel.addRange(range)
+    } else if (typeof document.body.createTextRange != "undefined") {
+        var textRange = document.body.createTextRange()
+        textRange.moveToElementText(el)
+        textRange.collapse(false)
+        textRange.select()
+    }
+}
+
 export default class InputPost extends Component {
 
   static propTypes = {
@@ -163,6 +181,7 @@ export default class InputPost extends Component {
   onFocus() {
     const { currentHashTag } = this.props
     this._input.innerHTML === '' && currentHashTag ? this._input.innerHTML = `#${currentHashTag} ` : ''
+    placeCaretAtEnd(this._input)
   }
 
   onKeyDown(event) {
