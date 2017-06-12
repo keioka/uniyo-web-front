127d0d09ffc3cb0068e66a565e17f644737ac6e3
K <koka0828us@gmail.com>
2017-06-12 01:39:09 -0700

    Update style

--- src/components/sidebars/SidebarRightChannelUsers/index.jsx
+++ src/components/sidebars/SidebarRightChannelUsers/index.jsx
@@ -1,20 +1,73 @@
 import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
+import FaSearch from 'react-icons/lib/fa/search'
+
+import {
+  ListUserDonutGive,
+} from '../../'
 
 import {
   wrapper,
+  header,
+  headerNav,
+  headerNavActive,
+  ul,
+  inputSearch,
+  inputSearchWrapper,
+  listUser,
+  listUserLeft,
+  listUserRight,
+  listUserLeftImg,
+  listUserLeftText,
+  listUserLeftTitle,
+  listUserLeftSubtitle,
 } from './style'
 
-const SidebarRightChannelUsers = ({ channelUsers }) => {
-  return (
-    <div className={wrapper} >
-      <ul>
-        {channelUsers && channelUsers.map(user => {
-          return (<li>{user.name}</li>)
-        })}
-      </ul>
-    </div>
-  )
+class SidebarRightChannelUsers extends Component {
+
+  get usersDonutsToGive() {
+    const { channelUsers, channelCreate, allChannels } = this.props
+    return channelUsers.map(user =>
+      <ListUserDonutGive
+        {...user}
+        channelCreate={channelCreate}
+        channels={allChannels}
+      />
+    )
+  }
+
+  render() {
+    const { channelUsers } = this.props
+    return (
+      <div className={wrapper}>
+        <div className={header}>
+          <h3>All user in this channel</h3>
+        </div>
+        <div className={ul}>
+          <div className={inputSearchWrapper}>
+            <FaSearch />
+            <input
+              type="text"
+              className={inputSearch}
+              placeholder="Search in your campus"
+              onChange={event => { this.setState({ userSearchQuery: event.target.value }); userSearch({ query: event.target.value }) }}
+            />
+          </div>
+          <div className={listUser}>
+            <div className={listUserLeft}>
+              <div className={listUserLeftImg}></div>
+              <div className={listUserLeftText}>
+                <span className={listUserLeftTitle}>Add Friend</span>
+              </div>
+            </div>
+          </div>
+          <ul>
+            {this.usersDonutsToGive}
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default SidebarRightChannelUsers
