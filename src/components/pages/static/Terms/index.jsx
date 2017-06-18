ec3629397312d3bac90f9c4a8506757a72bef487
K <koka0828us@gmail.com>
2017-06-18 16:24:03 -0700

    Add privacy and terms page

--- src/components/pages/static/Terms/index.jsx
+++ src/components/pages/static/Terms/index.jsx
@@ -1,5 +1,105 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
 
-export default () => (
-  <div>Terms</div>
-)
+import {
+  page,
+  container,
+  emoji,
+} from './style'
+
+export default class Terms extends Component {
+  render() {
+    return (
+      <div className={page}>
+
+        <div className={emoji}>🤓</div>
+
+        <div className={container}>
+          <h1>UniYo, Inc.<br/>Terms of Service</h1>
+          <p>EFFECTIVE DATE: March 21st, 2017</p>
+          <p>UniYo is a proprietary messaging application for educational institutions.</p>
+          <p>These UniYo Terms of Service (the “Terms”) are a legal contract between you and UniYo, Inc. (“UniYo”, “we” or “us”). These Terms explain how you are permitted to use the UniYo applications and services and any content therein (collectively, the “Services”).</p>
+          <p>Unless otherwise specified, all references to the Services include the services available through the UniYo web or mobile applications (collectively the “Apps”), as well as any software that UniYo provides to you that allows you to access the Services from a mobile device and any other Materials (as defined below). By using the Services, you are agreeing to all of the Terms; if you do not agree with any of these Terms, do not access or otherwise use the Services. You may download and/or print a copy of these Terms for you records.</p>
+          <p>In these Terms, "you", "member", "members" and "user" refer to the legal owner and/or the permanent user of the email address used to join UniYo.</p>
+          <p>The Terms determine our mutual obligations in connection with the use of our service. It is the responsibility of the member to keep his/her access codes to the Services (e-mail address and password) confidential. If the member has reason to believe that his/her account is not secured anymore, the member must, as soon as possible, change his/her password. The member is solely responsible for his/her account and all communications and actions taken with it.</p>
+          <p><strong>Preamble</strong></p>
+          <p>We are very concerned about the protection and respect of your personal data. We ask you only for information that we consider to be useful to share. Your own interest is also ours and the Services is yours. We remain constantly at your disposal for any questions regarding these Terms.</p>
+          <p><strong>Changes</strong></p>
+          <p>UniYo may make changes to the Services at any time. UniYo can change, update, or add or remove provisions of these Terms at any time by publishing the updated Terms by posting a notice on the Services. By using the Services after UniYo has updated the Terms, you are agreeing to all the updated Terms; if you do not agree with any of the updated Terms, you must stop using the Services.</p>
+          <p><strong>General Use</strong></p>
+          <p>By using the Services, you represent, acknowledge and agree that you are at least 13 years of age. If you are not at least 13 years old, you may not use the Services at any time or in any manner or submit any information to the Apps.</p>
+          <p>We may, in our sole discretion, refuse to offer the Services to any person or entity. We may, without notice and in our sole discretion, terminate your right to use the Services, or any portion thereof, and block or prevent your future access to and use of the Services or any portion thereof.</p>
+          <p><strong>Services</strong></p>
+          <p>UniYo provides content through the Services that is copyrighted and/or trademarked work of UniYo or UniYo’s third-party licensors and suppliers or other users of the Services (collectively, the “Materials”). Materials may include logos, text graphics, video, images, photos, software and other content.</p>
+          <p>Subject to these Terms, UniYo hereby grants you a limited, non-exclusive, non-licensable, non-transferable and revocable license to use and to display the Materials and to use the Services solely for your personal, non-commercial use. Except for the foregoing license, you have no other rights in the Services or any Materials and you may not modify, edit, copy, reproduce, create derivative works of, reverse engineer, alter, enhance or in any way exploit any of the Services or Materials in any manner.</p>
+          <p>You may not: (i) modify, disassemble, decompile or reverse engineer the Apps, except to the extent that such restriction is expressly prohibited by law; (ii) rent, lease, loan, resell, sublicense, distribute or otherwise transfer the Apps to any third-party or use the Apps to provide time sharing or similar services for any third-party; (iii) make any copies of the Apps; (iv) remove, circumvent, disable, damage or otherwise interfere with security-related features of the Apps, features that prevent or restrict use or copying of any content accessible through the Apps, or features that enforce limitations on use of the Apps; or (v) delete the copyright and other proprietary rights notices on the Apps. You acknowledge that UniYo may from time to time issue upgraded versions of the Apps, and may automatically electronically upgrade the version of the Apps that you are using on your devices. You consent to such automatic upgrading on your devices, and agree that these Terms will apply to all such upgrades. Standard carrier data charges may apply to your use of the App.</p>
+          <p>Except for the limited rights expressly licensed in these Terms, UniYo and its third-party licensors or suppliers retain all right, title, and interest in and to the Services.</p>
+          <p><strong>Privacy</strong></p>
+          <p>Please review UniYo’s Privacy Policy (the “Privacy Policy”), available at www.uniyo.io/privacy which explains how we use and disclose information that we collect from and about you. By using our Services, you agree that we may use and disclose the information we collect from and about you as stated in the Privacy Policy (which is incorporated into these Terms). Without limiting the foregoing, you expressly acknowledge that any Submissions (defined below) or other information you submit to the Services may be viewable by all other users of the Services and any third party.</p>
+          <p><strong>Submissions</strong></p>
+          <p>You are responsible for any information, profiles, opinions, messages, comments, photos, videos, graphics, sounds and other content or material that you submit, upload, post or otherwise make available on, through or in connection with the Services (each a “Submission”). When you provide Submissions you agree that those Submissions shall not be in violation of the “Unauthorized Activities” section below. You have full responsibility for each Submission you make, including its legality, reliability and appropriateness and will be solely liable for any damage or harm resulting from your Submissions.</p>
+          <p>We may retain your Submissions, even after they have expired from view within the Services or even after you have deleted them. However, you agree that we have no obligation to retain any Submission for any period of time, nor shall we be responsible or liable to you or any third party for any lost content or losses related to the expiration or deletion of a Submission.</p>
+          <p><strong>Unauthorized Activities</strong></p>
+          <p>When using the Services, you agree not to:</p>
+          <ul>
+            <li>Impersonate another person in a manner that is intended to or does mislead, confuse or deceive others.</li>
+            <li>Post or share another individual’s private information without their express authorization and permission.</li>
+            <li>Defame, abuse, bully, harass, stalk, threaten, or otherwise violate the legal rights of others.</li>
+            <li>Use racially or ethnically offensive language.</li>
+            <li>Discuss or incite illegal activity.</li>
+            <li>Post or share Submissions that contain pornography or graphic violence.</li>
+            <li>Post or share anything that exploits children or minors or that depicts cruelty to animals.</li>
+            <li>Post or share Submissions that violate any third party right, including any copyright, trademark, patent, trade secret, moral right, privacy right, right of publicity or any other intellectual property or proprietary right.</li>
+            <li>Disseminate any unsolicited or unauthorized advertising, promotional materials, 'junk mail', 'spam', 'chain letters', 'pyramid schemes', or any other form of such solicitation.</li>
+            <li>Use any robot, spider, crawler, scraper or other automated means to access the Services.</li>
+            <li>Take any action that imposes an unreasonable or disproportionately large load on our infrastructure.</li>
+            <li>Use or develop any third party applications that interact with the Services or Submissions without our prior written consent.</li>
+            <li>Alter the opinions or comments posted by others on the Services.</li>
+            <li>Post or share any image or language that is obscene, vulgar or offensive or that threatens, disparages or demeans any individual or group.</li>
+            <li>Attempt to circumvent any of our content-filtering techniques.</li>
+            <li>Post or share anything inappropriate or disruptive to the Services.</li>
+            <li>Disrupt, negatively affect or inhibit users from having a positive experience with the Services.</li>
+            <li>Use the Services in violation of these Terms and/or for any unlawful purposes.</li>
+            <li>Post or share anything contrary to our public image, goodwill or reputation.</li>
+          </ul>
+          <p>This list of prohibitions provides examples and is not exhaustive or exclusive. UniYo reserves the right to (a) suspend or terminate access to your account and your ability to post to the Services (or otherwise use the Services) and/or (b) delete, remove or refuse to distribute any Submissions, all with or without cause and with or without notice, for any reason or no reason without liability to you. UniYo may report to law enforcement authorities any actions that may be illegal, and any reports it receives of such conduct. When legally required or at UniYo’s discretion, UniYo will cooperate with law enforcement agencies in any investigation of alleged illegal activity on the Services or on the Internet.</p>
+          <p>Unauthorized use of any Materials or Third-Party Content contained in the Services may violate certain laws and regulations. You agree to indemnify and hold UniYo and its officers, directors, employees, consultants, affiliates, agents, licensors, and business partners (collectively, the “Indemnified Entities”) harmless from and against any and all costs, damages, liabilities, and expenses (including attorneys’ fees and costs of defense) UniYo or any other Indemnified Entity suffers in relation to, arising from, or for the purpose of avoiding, any claim or demand from a third party that your use of the Services or the use of the Services by any person using your user name violates any applicable law or regulation, or the copyrights, trademark rights or other rights of any third party.</p>
+          <p><strong>Disclaimer of Warranties</strong></p>
+          <p>Your use of the Services is at your own risk. The Materials may not have been verified or authenticated in whole or in part by UniYo, and they may include inaccuracies or typographical or other errors. UniYo does not warrant the accuracy of timeliness of the Materials contained on the Services. UniYo has no liability for any loss of, or errors or omissions in Submissions, or for any errors or omissions in the Materials or any other portion of the Services, whether provided by UniYo, our licensors or suppliers or other users.</p>
+          <p>UniYo, for itself and its licensors, makes no express, implied or statutory representations, warranties, or guarantees in connection with the services or any materials relating to the quality, suitability, truth, accuracy or completeness of any information or material contained or presented on the services. Unless otherwise explicitly stated, to the maximum extent permitted by applicable law, the services, materials and any other portion of the services or any information or material contained or presented on the services is provided to you on an “as is,” “as available” basis with no warranty of implied warranty of merchantability, fitness for a particular purpose, or non-infringement of third party rights. Without limiting the foregoing, UniYo does not provide any warranties against viruses, spyware or malware that may be installed on your computer.</p>
+          <p><strong>Limitation of Liability</strong></p>
+          <p>In no event will UniYo be liable for any indirect, special, punitive, incidental, exemplary, or consequential damages even if UniYo has been advised of the possibility of such damages. Without limiting the foregoing, UniYo shall not be liable to you for any damages resulting from your displaying, copying, or downloading any materials to or from the Services. You assume total responsibility for your use of the Services. Your only remedy against UniYo for dissatisfaction with the Services is to stop using the Services.</p>
+          <p><strong>Local Laws; Export Control</strong></p>
+          <p>UniYo controls and operates the Services from its headquarters in the United States and the Services may not be appropriate or available for use in other locations. You are responsible for following applicable laws and regulations in your jurisdiction, including but not limited to laws and regulations regarding the transmission of technical data exported from the United States or the country in which you reside.</p>
+          <p><strong>Feedback</strong></p>
+          <p>If you send or transmit any communications, comments, questions, suggestions, or related materials regarding UniYo or the Services, whether by email otherwise (collectively, “Feedback”), such Feedback is, and will be treated as, non-confidential and non-proprietary. You hereby assign all right, title, and interest in, and UniYo is free to use, without any attribution or compensation to you, any and all Feedback for any purpose whatsoever. You understand and agree that UniYo is not obligated to use, display, reproduce, or distribute any such ideas, know-how, concepts, or techniques contained in the Feedback, and you have no right to compel such use, display, reproduction, or distribution.</p>
+          <p><strong>General</strong></p>
+          <p>California state law and applicable U.S. federal law, without regard to the choice or conflicts of law provisions, will govern these Terms. Foreign laws do not apply. The United Nations on Contracts for the International Sale of Goods and any laws based on the Uniform Computer Information Transactions Act (UCITA) shall not apply to this Agreement. Any disputes relating to these Terms or the Services will be heard in the courts located in San Francisco, California. If any of these Terms is found to be inconsistent with applicable law, then such term shall be interpreted to reflect the intentions of the parties, and no other terms will be modified. UniYo’s failure to enforce any of these Terms is not a waiver of such term. These Terms are the entire agreement between you and UniYo and supersede all prior or contemporaneous negotiations, discussions or agreements between you and UniYo about the Services. The proprietary rights, disclaimer of warranties, representations made by you, indemnities, limitations of liability and general provisions shall survive any termination of these Terms.</p>
+          <p><strong>Copyright Policy</strong></p>
+          <p>UniYo respects the intellectual property rights of others. If you believe that your intellectual property appears on the Services in violation of your copyright, please provide UniYo designated agent the following information:</p>
+          <ul>
+            <li>A physical or electronic signature of a person authorized to act on behalf of the owner of an exclusive right that is allegedly infringed.</li>
+            <li>Identification of the work claimed to have been infringed, or, if multiple works at a single online site are covered by a single notification, a representative list of such works at that site.</li>
+            <li>Identification of the material that is claimed to be infringing or to be the subject of infringing activity and that is to be removed or access to which is to be disabled at the Services, and information reasonably sufficient to permit UniYo to locate the material.</li>
+            <li>Information reasonably sufficient to permit UniYo to contact you as the complaining party, such as an address, telephone number, and, if available, an electronic mail address at which you may be contacted.</li>
+            <li>A statement that you have a good faith belief that use of the material in the manner complained of is not authorized by the intellectual property owner, its agent, or the law.</li>
+            <li>A statement that the information in the notification is accurate, and under penalty of perjury, that you are authorized to act on behalf of the owner of an exclusive right that is allegedly infringed.</li>
+          </ul>
+          <p>UniYo’s agent for notice of claims of intellectual property infringement on the Services can be reached as follows:</p>
+          <p><strong>UniYo, Inc.</strong><br/>c/o The Refiners<br/>San Francisco, CA, 94103<br/><a href="hello@uniyo.io">hello@uniyo.io</a></p>
+          <p>We will notify you that we have removed or disabled access to copyright-protected material that you provided, if such removal is pursuant to a valid DMCA take-down notice that we have received. If you receive such notice from us, you may provide us with a counter-notification in writing to UniYo designated agent that includes all of the following information:</p>
+          <ul>
+            <li>Your physical or electronic signature;</li>
+            <li>Identification of the material that has been removed or to which access has been disabled, and the location at which the material appeared before it was removed or access to it was disabled;</li>
+            <li>A statement from you under the penalty of perjury, that you have a good faith belief that the material was removed or disabled as a result of a mistake or misidentification; and</li>
+            <li>Your name, physical address and telephone number, and a statement that you consent to the jurisdiction of a court for the judicial district in which your physical address is located, or if your physical address is outside of the United States, for any judicial district in which UniYo may be located, and that you will accept service of process from the person who provided notification of allegedly infringing material or an agent of such person.</li>
+          </ul>
+          <p><strong>Contact Us</strong></p>
+          <p>If you have any questions about these Terms or otherwise need to contact UniYo for any reason, you can reach us at <a href="hello@uniyo.io">hello@uniyo.io</a></p>
+        </div>
+
+        <div className={emoji}>🙌</div>
+
+      </div>
+    )
+  }
+}
