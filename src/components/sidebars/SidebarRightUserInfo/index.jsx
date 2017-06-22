341dc804f488c1d156ac8d485722ca1811e6c336
K <koka0828us@gmail.com>
2017-06-21 17:03:39 -0700

    Modify message button procedure

--- src/components/sidebars/SidebarRightUserInfo/index.jsx
+++ src/components/sidebars/SidebarRightUserInfo/index.jsx
@@ -25,61 +25,65 @@ const uniq = (array, param) => {
   })
 }
 
-const userInfo = ({ user, channelCreate, channels, userGiveDonuts }) => {
+export default class SidebarRightUserInfo extends Component {
+  render() {
+    const { allUsers, channelCreate, channels, userGiveDonuts, userId } = this.props
+    const user = allUsers.filter(user => user.id === userId)[0]
+    const onClickBtnMessage = () => {
+      const filteredChannel = channels.filter(channel => {
+        // check if current user has channel with the other user
+        // check channel is not group because it is supposed to be 1 to 1 chat
+        // check if the other user id is included. [0] is the other user and [1] is current user
+        return channel.users.length === 2 && channel.users[0].id == user.id
+      })
+      const channel = filteredChannel[0]
 
-  const onClickBtnMessage = () => {
-    const filteredChannel = channels.filter(channel => {
-      // check if current user has channel with the other user
-      // check channel is not group because it is supposed to be 1 to 1 chat
-      // check if the other user id is included. [0] is the other user and [1] is current user
-      return channel.users.length === 2 && channel.users[0].id == user.id
-    })
-    const channel = filteredChannel[0]
-
-    if (channel) {
-      browserHistory.push(`/dashboard/channels/${channel.id}`)
-    } else {
-      channelCreate({ users: [user.id] })
+      if (channel) {
+        browserHistory.push(`/dashboard/channels/${channel.id}`)
+      } else {
+        channelCreate({ users: [user.id] })
+      }
     }
-  }
 
-  const uniqueHashtagsCurrentUser = user.hashtags && uniq(user.hashtags, 'hashtag')
+    const uniqueHashtagsCurrentUser = user.hashtags && uniq(user.hashtags, 'hashtag')
+    return (
+      <div className={wrapper} >
+        {user ?
+          <div>
+            <div className={boxImg}>
+              <img className={imageProfile} src={user.image.largeUrl} alt=""/>
+              <div className={profile}>
+                <div className={profileName}>
+                  <h3 className={profileNameH3}>{user.firstName} {user.lastName}</h3>
+                </div>
+                <div className={profileNav}>
+                  <button className={btnMessage} onClick={onClickBtnMessage}>Message</button>
+                  <ButtonDonut
+                    onClick={() => userGiveDonuts({ userId: user.id, amount: 1 })}
+                    donutsCount={user.receivedDonutsCount}
+                  />
+                </div>
+              </div>
+            </div>
 
-  return (
-    <div className={wrapper} >
-      <div className={boxImg}>
-        <img className={imageProfile} src={user.image.largeUrl} alt=""/>
-        <div className={profile}>
-          <div className={profileName}>
-            <h3 className={profileNameH3}>{user.firstName} {user.lastName}</h3>
+            <div className={tags}>
+              <ul className={tagsList}>
+                {uniqueHashtagsCurrentUser && uniqueHashtagsCurrentUser.map(hashtag => <li className={tagsItem}><Link to={`dashboard?hashtag=${hashtag.hashtag}`}>#{hashtag.hashtag}</Link></li>)}
+              </ul>
+            </div>
           </div>
-          <div className={profileNav}>
-            <button className={btnMessage} onClick={onClickBtnMessage}>Message</button>
-            <ButtonDonut
-              onClick={() => userGiveDonuts({ userId: user.id, amount: 1 })}
-              donutsCount={user.receivedDonutsCount}
-            />
-          </div>
+          : null}
         </div>
-      </div>
-
-      <div className={tags}>
-        <ul className={tagsList}>
-          {uniqueHashtagsCurrentUser && uniqueHashtagsCurrentUser.map(hashtag => <li className={tagsItem}><Link to={`dashboard?hashtag=${hashtag.hashtag}`}>#{hashtag.hashtag}</Link></li>)}
-        </ul>
-      </div>
-    </div>
-  )
-}
-
-userInfo.defaultProps = {
-  user: {
-    hashtags: [],
-    name: '',
-    image: {
-      largeURL: 'https://uniyo.s3.amazonaws.com/users/profile/large/541_f53b1f4364a4415ebdbda4dd0af1ca51.jpg'
+      )
     }
   }
-}
 
-export default userInfo
+  SidebarRightUserInfo.defaultProps = {
+    user: {
+      hashtags: [],
+      name: '',
+      image: {
+        largeURL: 'https://uniyo.s3.amazonaws.com/users/profile/large/541_f53b1f4364a4415ebdbda4dd0af1ca51.jpg'
+      }
+    }
+  }
