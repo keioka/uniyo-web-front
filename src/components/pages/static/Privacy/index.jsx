4acfd2c8e8d7fa966a7a0a81a7a4ee88ed9ea18a
Emmanuel Darmon <manudarmon@gmail.com>
2017-06-29 18:08:59 +0200

    Replace UniYo with Uniyo

--- src/components/pages/static/Privacy/index.jsx
+++ src/components/pages/static/Privacy/index.jsx
@@ -13,10 +13,10 @@ export default class Privacy extends Component {
         <div className={emoji}>🤓</div>
 
         <div className={container}>
-          <h1>UniYo, Inc.<br/>Privacy Policy</h1>
+          <h1>Uniyo, Inc.<br/>Privacy Policy</h1>
           <p>EFFECTIVE DATE: March 21st, 2017</p>
-          <p>The UniYo applications (the “Apps”) by UniYo, Inc. and any content therein (collectively, the “Services”) offers students, professors and university staff members the opportunity to communicate with one another. In this Privacy Policy (“Policy”), we describe how UniYo, Inc. collects, uses, and discloses information that we obtain about users of our Services.</p>
-          <p>UniYo Inc. (“UniYo”) operates a messaging application dedicated to educational institutions. It is UniYo’s policy to respect your privacy regarding any information we may collect while operating our websites.</p>
+          <p>The Uniyo applications (the “Apps”) by Uniyo, Inc. and any content therein (collectively, the “Services”) offers students, professors and university staff members the opportunity to communicate with one another. In this Privacy Policy (“Policy”), we describe how Uniyo, Inc. collects, uses, and discloses information that we obtain about users of our Services.</p>
+          <p>Uniyo Inc. (“Uniyo”) operates a messaging application dedicated to educational institutions. It is Uniyo’s policy to respect your privacy regarding any information we may collect while operating our websites.</p>
           <p>Below is our privacy policy, which incorporates the following goals:</p>
           <ul>
             <li>We don’t ask you for personal information unless we truly need it. (We can’t stand services that ask you for things like your gender or income level for no apparent reason.)</li>
@@ -27,7 +27,7 @@ export default class Privacy extends Component {
           <p><strong>Account Information.</strong> When you first launch the Apps, you sign up using your email address and you need to provide your full name. You will also have a short profile that can include a photo and the newsfeeds that you are following.</p>
           <p>We may also collect the following information:</p>
           <p><strong>Your Content and Usage.</strong> We collect any content you submit to the Services, information about the content, such as when you submitted it, and any metadata that is provided with the content.</p>
-          <p>Other Information You Provide to Us. When you interact with our Services or otherwise communicate with UniYo in any way, we will collect the information you choose to share with us.</p>
+          <p>Other Information You Provide to Us. When you interact with our Services or otherwise communicate with Uniyo in any way, we will collect the information you choose to share with us.</p>
           <p><strong>Information We Collect Automatically.</strong> We may automatically collect information about your use of the Services such as: access time, device ID, application ID or other unique identifier; domain name, IP address, screen views; language information; device name and model, type of browser, location; operating system information; your activities within the Services; and the length of time that you are logged in. We may combine this information with other information that we have collected about you.</p>
           <p><strong>How We Use Information We Collect</strong></p>
           <p>In general, we use the information we collect from and about you to provide and operate the Services, respond to your requests, and to provide customer service and technical support. We may also use information:</p>
@@ -44,15 +44,15 @@ export default class Privacy extends Component {
           </ul>
           <p>To better understand how users access and use our Services, both on an aggregated and individualized basis, in order to improve our Services and respond to user desires and preferences, and for other research and analytical purposes.</p>
           <p><strong>Where Information is Processed</strong></p>
-          <p>UniYo, Inc. is based in the United States. No matter where you are located, you consent to the processing and transferring of your information in and to the U.S. and other countries. The laws of the U.S. and other countries governing data collection and use may not be as comprehensive or protective as the laws of the country where you live. And, your information may be made available to government or law enforcement requests.</p>
+          <p>Uniyo, Inc. is based in the United States. No matter where you are located, you consent to the processing and transferring of your information in and to the U.S. and other countries. The laws of the U.S. and other countries governing data collection and use may not be as comprehensive or protective as the laws of the country where you live. And, your information may be made available to government or law enforcement requests.</p>
           <p><strong>How We Share Information</strong></p>
           <p>Your profile is publicly available to all other members of your campus, as are your discussion on public newsfeeds. Nevertheless, discussions in the private messages section remain strictly private and cannot be seen by other members of the campus who do not partake in the discussion.</p>
           <p>The information we collect, including personally identifiable information, may be shared as follows:</p>
           <ul>
-            <li><strong>UniYo Users.</strong> Any information that you (i) post (ii) link to or (iii) send may be shared with other users in the same campus, depending on the type of feature that you are using. Any content that you send directly to another user in a private message will be shared solely with that user. Please don’t post content or send messages you wouldn’t want others to save or share.</li>
+            <li><strong>Uniyo Users.</strong> Any information that you (i) post (ii) link to or (iii) send may be shared with other users in the same campus, depending on the type of feature that you are using. Any content that you send directly to another user in a private message will be shared solely with that user. Please don’t post content or send messages you wouldn’t want others to save or share.</li>
             <li><strong>Third Parties and Partners.</strong> We may share the information with our third party providers, including and research and analytics providers.</li>
             <li><strong>In Response to Legal Process.</strong> We also may disclose the information in order to comply with the law, a judicial proceeding, court order, subpoena, or other legal process. For example, we may share your IP address or other identifier, your location information, or any other information we have collected about you and your device.</li>
-            <li><strong>To Protect Us and Others.</strong> We also may disclose information if we believe it is necessary to investigate, prevent, or take action regarding illegal activities, suspected fraud, situations involving potential threats to the safety of any person, violations of our Terms of Service or this Policy, or as evidence in litigation in which UniYo is involved.</li>
+            <li><strong>To Protect Us and Others.</strong> We also may disclose information if we believe it is necessary to investigate, prevent, or take action regarding illegal activities, suspected fraud, situations involving potential threats to the safety of any person, violations of our Terms of Service or this Policy, or as evidence in litigation in which Uniyo is involved.</li>
             <li><strong>Aggregate and De-Identified Information.</strong> We may also share aggregate or de-identified information about users with third parties for marketing, fundraising, advertising, research or similar purposes.</li>
           </ul>
           <p><strong>Push Notifications/Alerts</strong></p>
@@ -60,7 +60,7 @@ export default class Privacy extends Component {
           <p><strong>Third-Party Links</strong></p>
           <p>Our Services may contain third party links, including third party integrations. Any access to and use of such links or third party integrations is not governed by this Policy, but instead is governed by the privacy policies of those third parties.</p>
           <p><strong>Business Transfers</strong></p>
-          <p>If UniYo, or substantially all of its assets, were acquired, or in the unlikely event that UniYo goes out of business or enters bankruptcy, user information would be one of the assets that is transferred or acquired by a third party. You acknowledge that such transfers may occur, and that any acquirer of UniYo may continue to use your personal information as set forth in this policy.</p>
+          <p>If Uniyo, or substantially all of its assets, were acquired, or in the unlikely event that Uniyo goes out of business or enters bankruptcy, user information would be one of the assets that is transferred or acquired by a third party. You acknowledge that such transfers may occur, and that any acquirer of Uniyo may continue to use your personal information as set forth in this policy.</p>
           <p><strong>Minors</strong></p>
           <p>You must be at least 13 to use our Services. If you are not at least 13, please do not use our Services or provide any information to us. If we discover that a child under 13 has provided us with personal information, we take steps to remove such information from our systems.</p>
           <p><strong>Contact Us</strong></p>
