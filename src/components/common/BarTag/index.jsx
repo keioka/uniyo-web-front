493b90de0032870669a9108c4d8120f1e1c1a4e9
K <koka0828us@gmail.com>
2017-07-13 18:01:07 -0700

    Update BarTag for empty posts

--- src/components/common/BarTag/index.jsx
+++ src/components/common/BarTag/index.jsx
@@ -2,18 +2,32 @@ import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
 
 import {
-  barFilter,
+  barTag,
+  barTagInner,
+  barNav,
   btn,
   pink,
   yellow,
   blue,
   purple,
+  textFirstLine,
+  textSecondLine,
 } from './style'
 
 import Cross from './cross'
 import Plus from './plus'
 
+const BarTagInner = ({ firstLine, secondLine }) => {
+  return (
+    <div className={barTagInner} >
+      <div className={textFirstLine}>{ firstLine }</div>
+      <div className={textSecondLine}>{ secondLine }</div>
+    </div>
+  )
+}
+
 const BarTag = ({
+  empty,
   type,
   currentPostType,
   hashtag,
@@ -49,23 +63,75 @@ const BarTag = ({
     })
   }
 
-  const classNames = [ barFilter, color ]
+
+  const message = (tag) => {
+    switch (currentPostType) {
+      case 'QUESTION': {
+        const firstLineMessage = hashtag ? `🤷‍ Ask a question about #${hashtag} on your campus` : '🤷‍ Ask a question on your campus'
+        const secondLineMessage = 'You’ll get answers from other students'
+
+        return <BarTagInner
+          firstLine={firstLineMessage}
+          secondLine={secondLineMessage}
+        />
+      }
+
+      case 'REVIEW': {
+        const firstLineMessage = hashtag ? `😍  This is where you share reviews #${hashtag}` : 'This is where you share reviews'
+        const secondLineMessage =  hashtag ? `You can give 1 to 5 stars to #${hashtag} with a short description` : 'You can give 1 to 5 stars to #anytopic with a short description'
+
+        return <BarTagInner
+          firstLine={firstLineMessage}
+          secondLine={secondLineMessage}
+        />
+        break
+      }
+
+      case 'CLASS_NOTE': {
+        const firstLineMessage = hashtag ? `🙏 Here you can share a document about #${hashtag}` : 'Here you can share a document about any #topic'
+        const secondLineMessage = 'Your campus will love you for sharing your notes'
+
+        return <BarTagInner
+          firstLine={firstLineMessage}
+          secondLine={secondLineMessage}
+        />
+        break
+      }
+
+      default: {
+        const firstLineMessage = '🙌  This is where everything is happening on campus'
+        const secondLineMessage = hashtag ? `Make a first publication with a #${hashtag}` : 'Make a first publication'
+
+        return <BarTagInner
+          firstLine={firstLineMessage}
+          secondLine={secondLineMessage}
+        />
+      }
+    }
+
+  }
+
+  const classNames = [ barTag, color ]
   return (
     <div
       className={classNames.join(' ')}
       onClearCurrentTypeHandler={onClearCurrentTypeHandler}
     >
-      <span>#{hashtag}</span>
-      <Link
-        to={type ? `dashboard?type=${type}` : `dashboard`}
-        className={btn}
-      >
-        <Cross />
-      </Link>
-      <span className={btn} onClick={() => onAddHashtag()}>
-        <Plus />
-      </span>
-   </div>
+      { empty ? message(hashtag) : <span>#{hashtag}</span> }
+      { hashtag &&
+      <div className={barNav}>
+        <Link
+          to={type ? `dashboard?type=${type}` : `dashboard`}
+          className={btn}
+        >
+          <Cross />
+        </Link>
+        <span className={btn} onClick={() => onAddHashtag()}>
+          <Plus />
+        </span>
+     </div>
+     }
+  </div>
   )
 }
 
