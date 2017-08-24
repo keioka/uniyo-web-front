73accfcf7f1b08aa607dec495d27f375c901c2a6
K <koka0828us@gmail.com>
2017-08-24 10:45:27 +0900

    Add reducer reset

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,9 +2,23 @@ import { combineReducers } from 'redux'
 import { reducers } from 'uniyo-redux'
 import uiReducers from './ui/'
 import formReducers from './form/'
+import uiActionTypes from '../actionTypes'
 
-export default combineReducers({
+const appReducer = combineReducers({
   api: reducers,
   ui: uiReducers,
   form: formReducers,
 })
+
+const initialState = appReducer({}, {})
+
+const rootReducer = (state, action) => {
+  let nextState = state
+  if (action.type === uiActionTypes.signout.done) {
+    nextState = initialState
+  }
+
+  return appReducer(nextState, action)
+}
+
+export default rootReducer
