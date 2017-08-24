2b4f2000a3afe59e970a5ee13df34ae8cba0477f
K <koka0828us@gmail.com>
2017-08-24 10:45:44 +0900

    Add reducer reset

--- src/redux/sagas/app.js
+++ src/redux/sagas/app.js
@@ -8,12 +8,15 @@ import uiActionTypes from '../actionTypes'
 function* signoutFlow() {
   localStorage.clear()
   browserHistory.push('/')
+  yield put({ type: 'WS@STOP' })
+  yield put({ type: uiActionTypes.signout.done })
 }
 
 export function* signoutProcessSaga() {
   while(true) {
     yield take(uiActionTypes.signout.request)
-    yield take(actionTypes.deleteDevice.success)
+    // yield take(actionTypes.deleteDevice.success)
+    yield delay(1000)
     yield fork(signoutFlow)
   }
 }
