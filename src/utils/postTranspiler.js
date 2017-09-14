532f7b3876d8bc1f93fb4b7ac9e23f2e523192cd
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2017-09-14 17:25:07 +0430

    Trying to apply a quick fix to the nbsp problem. Issue: #288

--- src/utils/postTranspiler.js
+++ src/utils/postTranspiler.js
@@ -3,6 +3,7 @@ export default (textArea) => {
   text = textArea.innerHTML.replace(/\<span (\w+)[^>]+>[^>]+>/g, '').replace(/\<\/span+>/g, '').replace(/<br>/g, '\n')
   text = text.replace(/</g, '&lt;')
              .replace(/>/g, '&gt;')
+             .replace(/&nbsp;/, ' ')
 
   let userIds = textArea.innerHTML.match(/data-user-id\=\"[0-9]+\"/g)
 
