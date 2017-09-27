ff22237e108df604de6c312ddd666f6476bc7995
K <koka0828us@gmail.com>
2017-09-27 09:05:28 -0700

    Modify test

--- src/utils/__test__/localStoragehandler.test.js
+++ src/utils/__test__/localStoragehandler.test.js
@@ -6,17 +6,15 @@ import sinon from 'sinon'
 const EXPIRED_TIME = 7200
 
 describe('localStorageHandler', () => {
-  def('result', function() {
-    return {
-      accessToken: "casdsdasda",
-      refreshToken: "dsadsads",
-      expiresIn: EXPIRED_TIME,
-      user: {}
-    }
-  })
+  const result = {
+    accessToken: "casdsdasda",
+    refreshToken: "dsadsads",
+    expiresIn: EXPIRED_TIME,
+    user: {}
+  }
 
   beforeEach(function(){
-    storage.setTokensFromResponse($result)
+    storage.setTokensFromResponse(result)
   })
 
   it('should get accessToken', () => {
@@ -39,17 +37,15 @@ describe('localStorageHandler', () => {
 
 describe('localStorageHandler', () => {
   describe('is expired', () => {
-    def('result', function() {
-      return {
-        accessToken: "casdsdasda",
-        refreshToken: "dsadsads",
-        expiresIn: 0,
-        user: {}
-      }
-    })
+    const result = {
+      accessToken: "casdsdasda",
+      refreshToken: "dsadsads",
+      expiresIn: 0,
+      user: {}
+    }
 
     beforeEach(function(){
-      storage.setTokensFromResponse($result)
+      storage.setTokensFromResponse(result)
     })
 
     it('should get isAccessTokenExpired to be true', () => {
